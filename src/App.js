b673ff34cbc30840ccdc4d82f69e152d9e34776b
Matias <Micano1992@gmail.com>
2021-11-04 23:17:56 -0300

    Se agrega navBar con Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { BarraMenu } from './Componentes/NavBar/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>Prueba</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BarraMenu />
+
     </div>
   );
 }
---
+++ src/Componentes/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import react from 'react'
+import { Container } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import { Nav } from 'react-bootstrap'
+
+export const BarraMenu = () => {
+
+    return (
+
+        <Navbar bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">VIAJES-CANO</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#Mendoza">Mendoza</Nav.Link>
+                    <Nav.Link href="#TierraDelFuego">Tierra del fuego</Nav.Link>
+                    <Nav.Link href="#Salta">Salta</Nav.Link>
+                    <Nav.Link href="#Jujuy">Jujuy</Nav.Link>
+                    <Nav.Link href="#Chubut">Chubut</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+    )
+}
+
+//export default NavBar 
